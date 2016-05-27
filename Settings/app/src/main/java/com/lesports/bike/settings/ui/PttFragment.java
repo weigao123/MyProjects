4834e271b49876be5a0eb2f0270d37d215a6e005
gaowei3 <gaowei3@le.com>
2016-05-27 22:51:05 +0800

    modify ptt ui

--- Settings/app/src/main/java/com/lesports/bike/settings/ui/PttFragment.java
+++ Settings/app/src/main/java/com/lesports/bike/settings/ui/PttFragment.java
@@ -9,6 +9,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.lesports.bike.settings.R;
@@ -17,31 +22,41 @@ import com.lesports.bike.settings.widget.SwitchButton;
 import org.w3c.dom.Text;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 import bike.os.media.PTTManager;
 
 /**
  * Created by gwball on 2016/5/25.
  */
-public class PttFragment extends BaseFragment {
+public class PttFragment extends BaseFragment implements AdapterView.OnItemClickListener {
     private PTTManager mPttManager;
 
     private SwitchButton mSwitchButton;
     private TextView mStatusView;
+    private ArrayList<String> mChannelsList = new ArrayList<>();
 
     @Override
     protected void initViewAndData() {
         mStatusView = (TextView) getActivity().findViewById(R.id.ptt_status);
         mSwitchButton = (SwitchButton) getActivity().findViewById(R.id.ptt_switch);
 
+
+
         mPttManager = (PTTManager) getActivity().getSystemService("ptt_service");
         double[] allChannels = mPttManager.getAllChannels();
 
-        String[] ptts = new String[allChannels.length];
         DecimalFormat df = new DecimalFormat("#.0000");
         for (int i = 0; i < allChannels.length; i++) {
-            ptts[i] = df.format(allChannels[i]);
+            mChannelsList.add(df.format(allChannels[i]));
         }
+
+        ListView listView = (ListView) getActivity().findViewById(R.id.ptt_listview);
+        ListAdapter adapter = new ListAdapter(getActivity(), 0, mChannelsList);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+
+
 
         int pttStatus = mPttManager.getCurChannelId();
         Log.d("wei.gao", "ptt: "+pttStatus);
@@ -68,4 +83,35 @@ public class PttFragment extends BaseFragment {
         }
 
     };
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+
+    private class ListAdapter extends ArrayAdapter<String> {
+        public ListAdapter(Context context, int resource, ArrayList<String> objects) {
+            super(context, resource, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder viewHolder;
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.ptt_list_item, null);
+                viewHolder = new ViewHolder();
+                viewHolder.textView = (TextView) convertView.findViewById(R.id.ptt_item_channel);
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+            viewHolder.textView.setText(mChannelsList.get(position));
+            return convertView;
+        }
+    }
+
+    private static class ViewHolder {
+        public TextView textView;
+    }
 }
